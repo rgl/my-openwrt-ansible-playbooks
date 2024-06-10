64db555468598c0d2961d521a3a94c8d4871c464
Rui Lopes <rgl@ruilopes.com>
2024-06-10 11:52:12 +0100

    upgrade to ansible-core 2.17.0

--- ansible.sh
+++ ansible.sh
@@ -2,33 +2,33 @@
 set -euo pipefail
 
 command="$(basename "$0" .sh)"
-tag="ansible-$(basename "$PWD")"
 
 # build the ansible image.
-DOCKER_BUILDKIT=1 docker build -f Dockerfile.ansible -t "$tag" .
+install -d tmp
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.ansible --iidfile tmp/ansible-iid.txt .
+ansible_iid="$(cat tmp/ansible-iid.txt)"
 
 # show information about the execution environment.
-docker run --rm -i "$tag" bash <<'EOF'
+docker run --rm -i "$ansible_iid" bash <<'EOF'
 exec 2>&1
 set -euxo pipefail
 cat /etc/os-release
 ansible --version
 python3 -m pip list
 ansible-galaxy collection list
-ansible-galaxy role list
 EOF
 
 # execute command (e.g. ansible-playbook).
 # NB the GITHUB_ prefixed environment variables are used to trigger ansible-lint
 #    to annotate the GitHub Actions Workflow with the linting violations.
-#    see https://github.com/ansible/ansible-lint/blob/v6.13.1/src/ansiblelint/app.py#L85
+#    see https://github.com/ansible/ansible-lint/blob/v24.6.0/src/ansiblelint/app.py#L111-L112
 #    see https://ansible-lint.readthedocs.io/en/latest/usage/#ci-cd
 exec docker run \
     --rm \
     --net=host \
-    -v "$PWD:/playbooks:ro" \
+    -v "$PWD:/project:ro" \
     -e GITHUB_ACTIONS \
     -e GITHUB_WORKFLOW \
-    "$tag" \
+    "$ansible_iid" \
     "$command" \
     "$@"
